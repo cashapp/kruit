1e10de5011257c71e4287f268c376a8092bbd698
Geoff Flarity <geoff@squareup.com>
2019-08-06 16:09:39 -0400

    use the node names as vis network ids

--- sample_plugins/node_view/src/node_view.ts
+++ sample_plugins/node_view/src/node_view.ts
@@ -1,5 +1,5 @@
-import { V1ObjectMeta } from '@kubernetes/client-node';
-import { Kubernetes, KubernetesClientFactory, Watcher} from 'clustermuck';
+import { V1ObjectMeta, KubeConfig } from '@kubernetes/client-node';
+import { Kubernetes, KubernetesClientFactory, Watcher, newKubeConfig } from 'clustermuck';
 import * as http from "http"
 
 // TODO there should be type definitions in the near future, add them once they're available
@@ -10,39 +10,35 @@ export = (context: any): void => {
 }
 
 class NodeViewer {
-    idCount = 0   
+    kubeConfig = newKubeConfig()
     visNodes: vis.DataSet<vis.Node>  = new vis.DataSet([])
     visEdges: vis.DataSet<vis.Edge> = new vis.DataSet([])
-    allNodes = new Map<string, number>()
+    allNodes = new Map<string, string>()
     container = document.getElementById("container") as HTMLElement    
     network: vis.Network = new vis.Network(this.container, { nodes: this.visNodes, edges: this.visEdges}, {})
 
-    constructor() {        
+    constructor() {
         const self = this
-        // our vis graph nodes
-        const clusterVisNodeId: number = self.idCount
 
-        // TODO some documentation here
-        const factory = new KubernetesClientFactory()
-
+        const clusterVisNodeId = self.kubeConfig.currentContext
         // TODO make this a different colour
         // TODO get the actual name of the cluster somehow
         // create a cluster node to be connected to everything, this makes the network stabilization faster
-        self.visNodes.add({id: clusterVisNodeId, label: factory.getKubeConfig().currentContext, color: "#f7da00"  })
+        self.visNodes.add({id: clusterVisNodeId, label:clusterVisNodeId, color: "#f7da00"  })
 
-        const nodeWatcher = Watcher.newIWatcher(factory.getKubeConfig(), Kubernetes.V1Node)
+        const nodeWatcher = Watcher.newIWatcher(self.kubeConfig, Kubernetes.V1Node)
         nodeWatcher.on("ADDED", (node) => {
             const metadata = node["metadata"] as V1ObjectMeta
             const nodeName = metadata.name as string
-            self.visNodes.add({ id: ++self.idCount, label: nodeName,  shape: "box" })
-            self.allNodes.set(nodeName, self.idCount)
-            self.visEdges.add({ id: self.idCount, from: clusterVisNodeId, to: self.idCount })
+            self.visNodes.add({ id: nodeName, label: nodeName,  shape: "box" })
+            self.allNodes.set(nodeName, nodeName)
+            self.visEdges.add({ id: nodeName, from: clusterVisNodeId, to: nodeName })
         })
 
         nodeWatcher.on("DELETED", (node) => {
             const metadata = node["metadata"] as V1ObjectMeta
             const nodeName = metadata.name as string
-            const id = self.allNodes.get(nodeName) as number    
+            const id = self.allNodes.get(nodeName) as string
             self.visNodes.remove(id)
             self.visEdges.remove(id)
         })
