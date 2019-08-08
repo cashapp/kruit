69003faacd322ca071b28dfc917c2e3246124033
Geoff Flarity <geoff@squareup.com>
2019-08-07 21:35:45 -0400

    renamed watcher_factory.ts watcher.ts

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import * as Kubernetes from "@kubernetes/client-node"
 export { Kubernetes }
 export * from "./widgets"
 export * from "./kubernetes/client_factory"
-export * from "./kubernetes/watcher_factory"
+export * from "./kubernetes/watcher"
--- src/kubernetes/watcher.test.ts
+++ src/kubernetes/watcher.test.ts
@@ -1,5 +1,5 @@
 import * as k8s from "@kubernetes/client-node"
-import { Watcher } from './watcher_factory';
+import { Watcher } from './watcher';
 
 jest.mock("./watcher_factory")
 
